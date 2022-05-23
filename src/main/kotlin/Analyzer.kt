cceb3f5fd1c14f74f37a82e58900195026b4c9a8
Nada Feteiha <nada.feteiha@gmail.com>
2022-05-23 15:29:36 -0400

    "update analyzer class and appparser as we agree in meeting."

--- src/main/kotlin/Analyzer.kt
+++ src/main/kotlin/Analyzer.kt
@@ -1,2 +1,25 @@
-class Analyzer {
+import java.time.LocalDate
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+class Analyzer(private val apps: List<GoogleApp>) {
+
+    fun findOldestApp():String{
+
+        return apps[0].appName
+    }
+
+    private fun convertToDate(dateString: String): Long {
+        val dateList = dateString.split(" ")
+        val day = dateList[1].toInt()
+        val year = dateList[2].toInt()
+        val dateNewFormat = if (day<10) "${dateList[0]}-0$day-${year}" else "${dateList[0]}-$day-${year}"
+
+        val formatter = DateTimeFormatter.ofPattern("MMMM-dd-uuuu", Locale.ENGLISH)
+        val date = LocalDate.parse(dateNewFormat, formatter)
+
+        val millisecond: Long = date.atStartOfDay(ZoneOffset.MIN).toInstant().toEpochMilli()
+        return millisecond
+    }
 }
