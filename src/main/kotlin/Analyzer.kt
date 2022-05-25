c2a8037246f87451f67a71fcd3434e436a98e52b
muslim <n00rALDENakel>
2022-05-25 08:18:45 +0300

    add: function to find top ten app install.

--- src/main/kotlin/Analyzer.kt
+++ src/main/kotlin/Analyzer.kt
@@ -1,8 +1,23 @@
 class Analyzer() {
 
     fun findOldestApp(apps: List<App>): Double {
-
         return 0.0
     }
+
+
+    fun topTenAppInstall(appDataList: List<App>): List<String>? =
+        if (appDataList.isNotEmpty())
+            appDataList.groupBy { it.appName }.keys
+                .associateWith { appInstallCount(it, appDataList) }
+                .asSequence()
+                .sortedByDescending { (_, value) -> value }
+                .map { value -> value.key }
+                .take(10)
+                .toList()
+        else null
+
+    private fun appInstallCount(appName: String, appDataList: List<App>) =
+        appDataList.first { first -> first.appName == appName }.installs
+
 
 }
