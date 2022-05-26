7726097a01d9269bbcd4b1bc086bfb9fd3bbf11f
Nada Feteiha <nada.feteiha@gmail.com>
2022-05-26 14:01:42 -0400

    "reset develop branch "

--- src/test/kotlin/AnalyzerTest.kt
+++ src/test/kotlin/AnalyzerTest.kt
@@ -8,8 +8,9 @@ import utilities.Constant
 import utilities.convertStringToDate
 import java.text.SimpleDateFormat
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-internal class AnalyzerTest {
+internal class AnalyzerTest{
+
+
     private lateinit var analyzer: Analyzer
 
     @BeforeAll
@@ -48,61 +49,127 @@ internal class AnalyzerTest {
             "Google Drive",
             "Gboard - the Google Keyboard",
             "WhatsApp Messenger",
-            "Google Assistant")
-
+            "Google Assistant"
+        )
         assertEquals(predictable, result)
     }
 
 
     @Test
-    fun should_ReturnMinus1_When_EmptyList(){
+    fun should_ReturnMinus1_When_EmptyList() {
         //Given emptyList
         val app = listOf<App>()
         //when search for the oldest app
         val result = Analyzer().findOldestApp(app)
         //then check the result
-        assertEquals("-1",result)
+        assertEquals("-1", result)
     }
 
     @Test
-    fun should_Return_oldestApp_whenHasValidList(){
+    fun should_Return_oldestApp_whenHasValidList() {
         //Given emptyList
         val app = AppParser().parseFile(Constant.TEST_FILE_NAME)
         //when search for the oldest app
         val result = Analyzer().findOldestApp(app)
         //then check the result
-        assertEquals("The Kremer Collection VR Museum",result)
+        assertEquals("The Kremer Collection VR Museum", result)
     }
 
     @Test
-    fun should_Return_ConvertedDate_When_hasValidDate(){
+    fun should_Return_ConvertedDate_When_hasValidDate() {
         //Given date with correct format MMM DD YYYY
         val date = "May 15 2022"
         //when convert the stringDate to Date
         val result = convertStringToDate(date)
         //then check the result
-        assertEquals(SimpleDateFormat(Constant.DATE_FORMAT).parse("MAY-15-2022"),result)
+        assertEquals(SimpleDateFormat(Constant.DATE_FORMAT).parse("MAY-15-2022"), result)
     }
 
 
     @Test
-    fun should_Return_ConvertedDate_When_DayLessThen10(){
+    fun should_Return_ConvertedDate_When_DayLessThen10() {
         //Given date with different format day is not 05
         val date = "May 5 2022"
         //when convert StringDate to date
         val result = convertStringToDate(date)
         //then check the result
-        assertEquals(SimpleDateFormat(Constant.DATE_FORMAT).parse("MAY-05-2022"),result)
+        assertEquals(SimpleDateFormat(Constant.DATE_FORMAT).parse("MAY-05-2022"), result)
     }
 
     @Test
-    fun should_ThrowException_When_WrongDateFormat(){
+    fun should_ThrowException_When_WrongDateFormat() {
         //Given date with different format
         val date = "5 5 2022"
         //when convert StringDate to date
-        val wrongFormatException:Executable =Executable{ convertStringToDate(date)}
+        val wrongFormatException = Executable { convertStringToDate(date) }
         //then check the result
-        assertThrows(Exception::class.java,wrongFormatException)
+        assertThrows(Exception::class.java, wrongFormatException)
     }
 
+    @Test
+    fun should_ReturnNull_When_EmptyList() {
+        //given empty list
+        val appList = mutableListOf<App>()
+
+        // when searching for "9 and up"
+        val result = analyzer.percentageAppsRunningOnAndroid9(appList)
+
+        //then should return null
+        assertNull(result)
+    }
+
+    @Test
+    fun `should_ReturnZero_When_Don'tContains9`() {
+        // given list
+        val appList = App(
+            "Catholic Teachings",
+            "MissalDaily.com",
+            "Libraries & Demo",
+            convertStringToDate("November 7 2019"),
+            "11M",
+            1,
+            "3",
+            "4.4 and up"
+        )
+
+        // when searching for "9 and up"
+        val result = analyzer.percentageAppsRunningOnAndroid9(listOf(appList))
+
+        //then should return null
+        assertNull(result)
+    }
+
+    @Test
+    fun should_ReturnPercentage_When_Contains9() {
+        // given list
+        val appList = mutableListOf<App>()
+        appList.add(
+            App(
+                "Catholic Teachings",
+                "MissalDaily.com",
+                "Libraries & Demo",
+                convertStringToDate("November 7 2019"),
+                "11M",
+                1,
+                "3",
+                "4.4 and up"
+            )
+        )
+        appList.add(
+            App(
+                "Diamonds | Pets Rock", "TIMEFLIK (ex MR TIME)",
+                "Personalization",
+                convertStringToDate("August 18 2021"),
+                "4.7M",
+                10,
+                "1.0.0",
+                "9 and up"
+            )
+        )
+        // when searching for "9 and up"
+        val result = analyzer.percentageAppsRunningOnAndroid9(appList)
+
+        //then should return 50.00
+        assertEquals("50.0", result)
+    }
 }
