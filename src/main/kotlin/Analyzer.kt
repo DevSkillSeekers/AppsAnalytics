ca0f56f30089c4a42e623ba74a8d31dbfde3e3ad
Nada Feteiha <nada.feteiha@gmail.com>
2022-05-26 13:53:28 -0400

    "add test cases for find the oldest app "

--- src/main/kotlin/Analyzer.kt
+++ src/main/kotlin/Analyzer.kt
@@ -20,9 +20,11 @@ class Analyzer() {
     }
 
     fun getPercentageOfCategory(apps: List<App>,categoryName:String):Double{
-        return apps.filter { it.category.contains(categoryName,true) }
+        return if (apps.isNotEmpty())
+         apps.filter { it.category.contains(categoryName,true) }
             .size.div(apps.size.toDouble()).times(100)
             .times(100).roundToInt().toDouble() / 100
+        else -1.0
     }
     fun findAppsByCompany(companyName: String,apps: List<App>):Int{
 
@@ -31,13 +33,11 @@ class Analyzer() {
 
     }
 
-
-    // move string "Varies with device" to constant file
     fun getLargestApp(apps: List<App>,size:Int):List<App>?{
         if (apps.isNotEmpty()) {
             val list = mutableMapOf<App,Long>()
 
-             apps.filterNot { it.size.contains("Varies with device", true) }
+             apps.filterNot { it.size.contains("Varies", true) }
                  .apply {
                     onEach {
                        list[it] = convertToNumber(it.size)
@@ -45,7 +45,6 @@ class Analyzer() {
                  }
             return list.toList().sortedByDescending { (_, value) -> value}.toMap().keys.toList().take(size)
         }
-
         return null
     }
 
