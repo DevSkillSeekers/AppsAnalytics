2bc6bf8cbc90db614e8e5e0a484b284177b416b2
RAWAD <rawadjamal99@gmail.com>
2022-05-28 19:29:24 +0300

    add test cases to function "topTenLargestApp"

--- src/test/kotlin/AnalyzerTest.kt
+++ src/test/kotlin/AnalyzerTest.kt
@@ -1,19 +1,78 @@
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.function.Executable
 import parser.AppParser
 import utilities.Constant
 import utilities.convertStringToDate
 import java.text.SimpleDateFormat
+import java.util.*
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class AnalyzerTest{
 
     private lateinit var analyzer: Analyzer
+    private lateinit var apps: MutableList<App>
 
     @BeforeAll
     fun setup() {
         analyzer = Analyzer()
+    }
+
+    private fun initializeAppList(testListNumber: Int){
+        when (testListNumber) {
+            0 -> apps=  mutableListOf()
+            1 -> {
+                apps=  mutableListOf()
+                apps.add(
+                    App(
+                        "Books",
+                        "Amazon",
+                        "Libraries & Demo",
+                        Date("5/1/2000"),
+                        "21M", 500,
+                        "1.0.0",
+                        "4.4"
+                    )
+                )
+                apps.add(
+                    App(
+                        "AD",
+                        "Amazon",
+                        "Libraries & Demo",
+                        Date("1/1/2020"),
+                        "30M",
+                        500,
+                        "1.0.0",
+                        "9.0"
+                    )
+                )
+                apps.add(
+                    App(
+                        "Google Photo",
+                        "Google",
+                        "Libraries & Demo",
+                        Date("2/1/2000"),
+                        "50M",
+                        500,
+                        "1.0.0",
+                        "6.0"
+                    )
+                )
+                apps.add(
+                    App(
+                        "Google Files",
+                        "Google",
+                        "Medical",
+                        Date("1/1/2019"),
+                        "5M", 500,
+                        "1.0.0",
+                        "9.0"
+                    )
+                )
+            }
+        }
     }
 
     @Test
@@ -102,6 +161,18 @@ internal class AnalyzerTest{
         val wrongFormatException: Executable =Executable{ convertStringToDate(date)}
         //then check the result
         assertThrows(Exception::class.java,wrongFormatException)
+    }
+
+    @Test
+    fun should_Return_Top10LargestApp() {
+        //given list with data of the file & the number of value will return
+         initializeAppList(1)
+           var valueReturnNumber= 4
+        //when search for top 10 largest app
+           val functionResultValue = analyzer.topTenLargestApp(apps,valueReturnNumber)
+        //then should return that order of top 10 largest app that compares the size with unit KB
+           val expectedResultValue = listOf("Google Photo", "AD","Books", "Google Files")
+        assertEquals(expectedResultValue, functionResultValue)
     }
 
 }
