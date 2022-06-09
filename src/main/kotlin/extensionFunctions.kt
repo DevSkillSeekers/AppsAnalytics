56df7974b0d7c58a98fcb85fbf55fbc028102391
Nawfal J.Mahmood <88491921+naufalAliraqi@users.noreply.github.com>
2022-06-09 12:52:14 +0300

    Improve/convert string to mega byte function (#19)
    * replace 'else if' by 'when'
    
    * replace name of function

--- src/main/kotlin/extensionFunctions.kt
+++ src/main/kotlin/extensionFunctions.kt
@@ -1,19 +1,14 @@
 import java.text.SimpleDateFormat
 import java.util.*
 
-fun String.convertStringToSizeUnit() =
-    if (this == "Varies with device"){
-        0.0
+fun String.toMegaByteSize(): Double {
+    return when (this.uppercase().lastOrNull()) {
+        'M' -> this.replace("M", "").toDouble()
+        'K' -> this.replace("K", "").toDouble() / 1024.0
+        'G' -> this.replace("G", "").toDouble() * 1024.0
+        else -> 0.0
     }
-    else if (this.contains("M")) {
-        this.replace("M", "").toDouble()
-    } else if (this.contains("k")) {
-        this.replace("k", "").toDouble() / 1024.0
-    } else if (this.contains("G")) {
-        this.replace("G", "").toDouble() * 1024.0
-    } else {
-    0.0
-    }
+}
 
 //fun String.convertStringToLocalDate(): LocalDate {
 //    return LocalDate.parse(this, DateTimeFormatter.ofPattern("MMMM d yyyy"))
